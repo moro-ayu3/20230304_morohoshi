7ffdf68ab6f6d69040b406efa7e1d77fb816723a
moro-ayu3 <morohoshi3ayumi@gmail.com>
2023-03-05 21:30:53 +0900

    sixth-commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,48 +4,186 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yield('title')実践アプリ開発入門</title>
+  <title>実践アプリ開発入門</title>
+  <style>
+    .homepage {
+      width: 100%;
+      background-color: #191970;
+    }
 
-  @vite('css/reset.css/style.css')
+    .inner {
+      width: 700px;
+      height: 300px;
+      border-radius: 10px 10px 10px 10px;
+      background-color: #fff;
+      margin: 0 auto;
+    }
+
+    .header-title {
+      font-size: 30px;
+      font-weight: bold;
+      color: black;
+      margin-top: 30px;
+      margin-left: 20px;
+    }
+
+    .form {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+    }
+
+    .text {
+      width: 500px;
+      height: 30px;
+      border: solid 1px #c0c0c0;
+      margin-left: 30px;
+      border-radius: 5px 5px 5px 5px;
+      display: inline-block;
+    }
+
+    .create-btn {
+      width: 80px;
+      height: 40px;
+      margin-left: 30px;
+      margin-right: 20px;
+      border-radius: 10px 10px 10px 10px;
+      border: solid 3px #9966CC;
+      display: inline-block;
+      font-size: 15px;
+      font-weight: bold;
+      color: #9966CC;
+      padding:15px 20px;
+    }
+
+    table {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 20px;
+      margin-bottom: 40px;
+    }
+
+    .date-1 {
+      margin-left: 50px;
+      font-size: 20px;
+      font-weight: bold;
+      color: black;
+    }
+
+    .name {
+      margin-left: 80px;
+      font-size: 20px;
+      font-weight: bold;
+      color: black;
+    }
+
+    .update {
+      margin-left: 40px;
+      font-size: 20px;
+      font-weight: bold;
+      color: black;
+    }
+
+    .delete {
+      margin-left: 20px;
+      margin-right: 20px;
+      font-size: 20px;
+      font-weight: bold;
+      color: black;
+    }
+
+    .date-2 {
+      margin-left: 20px;
+      font-size: 20px;
+      color: black;
+    }
+
+    .input-update {
+      width: 200px;
+      height: 20px;
+      border: solid 1px #c0c0c0;
+      border-radius: 5px 5px 5px 5px;
+      margin-left: 10px;
+    }
+
+    .button-update {
+      width: 80px;
+      height: 40px;
+      border-radius: 10px 10px 10px 10px;
+      border: solid 3px #ffa500;
+      margin-left: 10px;
+      font-size: 15px;
+      font-weight: bold;
+      color: #ffa500;
+      padding: 15px 20px;
+    }
+
+    .button-delete {
+      width: 80px;
+      height: 40px;
+      border-radius: 10px 10px 10px 10px;
+      border: solid 3px #40e0d0;
+      margin-left: 10px;
+      margin-right: 20px;
+      font-size: 15px;
+      font-weight: bold;
+      color: #40e0d0;
+      padding: 15px 20px;
+    }
+
+  </style>
 </head>
 
 <body>
-  <h1>@yield('title'){{$todo}}</h1>
+  <h1>{{$todo}}</h1>
   <div class="homepage">
-    @yield('content')
      <div class="inner">
       <header>
         <h1 class="header-title">TodoList</h1>
       </header>
       <main>
         <div class="form">
-          <form action="/" method="get">
+          <form action="/todo/create" method="get">
           @csrf
             <input type="text" class="text">
+            <button class="create-btn">追加
+            </button>
           </form>
-          <div class="create-btn">
-            <p class="create-btn_text">追加</p>
-          </div>
         </div>
-        <div class="title">
-          <h2 class="date-1">作成日</h2>
-          <h2 class="name">タスク名</h2>
-          <h2 class="update">更新</h2>
-          <h2 class="delete">削除</h2>
-        </div>
-        <div class="content">
-          <div class="date-2">2023-03-03 01:08:52</div>
-          <form action="/" method="get">
-          @csrf
-            <input type="text" class="task">
-          </form>
-          <div class="update-btn">
-            <p class="update-btn_text">更新</p>
-          </div>
-          <div class="delete-btn">
-            <p class="delete-btn_text">削除</p>
-          </div>
-        </div>
+
+        <table>
+          <tr>
+            <th class="date-1">作成日</th>
+            <th class="name">タスク名</th>
+            <th class="update">更新</th>
+            <th class="delete">削除</th>
+          </tr>
+          @foreach($todos as $todo)
+          <tr>
+            <td>
+              {{ $todo->created_at}}
+            </td>
+              <div class="date-2">2023-03-03 01:08:52</div>
+              <form action="/todo/update" method="post">
+                @csrf
+                <td>
+                  <input type="text" class="input-update" value="" name="content" />
+                </td>
+                <td>
+                  <button class="button-update">更新</button>
+                </td>
+              </form>
+              <td>
+                <form action="/todo/delete" method="post">
+                  @csrf
+                  <button class="button-delete">削除</button>
+                </form>
+              </td>
+          </tr>
+          @endforeach
+        </table>
       </main>
      </div>
     </div>
@@ -53,23 +191,3 @@
 
 </html>
 
-@section('title', 'index.blade.php')
-
-@section('content')
-<table>
-  <tr>
-    <th>id</th>
-    <th>string</th>
-    <th>timestamp</th>
-    <th>timestamp</th>
-  </tr>
-  @foreach ($todos as $todo)
-  <tr>
-    <td>{{$todo->id}}</td>
-    <td>{{$todo->string}}</td>
-    <td>{{$todo->timestamp}}</td>
-    <td>{{$todo->timestamp}}</td>
-  </tr>
-  @endforeach
-</table>
-@endsection
