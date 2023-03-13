317fd5e429b6c71b8b49be145d7b203e86b90d1e
moro-ayu3 <morohoshi3ayumi@gmail.com>
2023-03-13 09:21:00 +0900

    thirty-fourth-commit

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController;
 
-Route::get('/', [TodoController::class, 'index']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::post('/todo/create', [TodoController::class, 'store']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::post('/todo/update', [TodoController::class, 'update']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::post('/todo/delete', [TodoController::class, 'delete']);
+require __DIR__.'/auth.php';
