5da1b50ef5766554041e66107f01fefae24a3d20
moro-ayu3 <morohoshi3ayumi@gmail.com>
2023-03-18 12:12:38 +0900

    fifty-third-commit

--- .history/app/Providers/RouteServiceProvider_20230318120551.php
+++ .history/app/Providers/RouteServiceProvider_20230318120551.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/';
+    public const HOME = '/dashboard';aa
 
     /**
      * The controller namespace for the application.
